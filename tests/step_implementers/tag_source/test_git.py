febef566f5d58d6f6cf5b9502ed9e979ad186603
Kevin J. Franklin <kfrankli@redhat.com>
2020-07-16 09:38:07 -0400

    wip

--- tests/step_implementers/tag_source/test_git.py
+++ tests/step_implementers/tag_source/test_git.py
@@ -1,8 +1,7 @@
-import os
-
 import pytest
 from testfixtures import TempDirectory
 import yaml
+import os
 
 from tssc import TSSCFactory
 from tssc.step_implementers.tag_source import Git
@@ -21,13 +20,36 @@ def test_tag_source_default():
 def test_tag_source_specify_git_implementer():
     with TempDirectory() as temp_dir:
         config = {
-            'tssc-config': {    
+            'tssc-config': {
                 'tag-source': {
                     'implementer': 'Git',
-                    'config': {}
+                    'config': {
+                        'username-env-var': 'GIT_USERNAME',
+                        'password-env-var': 'GIT_PASSWORD'
+                    }
                 }
             }
         }
-        expected_step_results = {'tssc-results': {'tag-source': {}}}
+        expected_step_results = {
+            'tssc-results': {
+                'generate-metadata': {
+                    'app-version': 'app_version',
+                    'pre-release': 'feature_test0',
+                    'build': 'build',
+                    'version': 'version',
+                    'image-tag': 'image_tag'
+                }
+            },
+            'tag-source': {}
+        }
+        #expected_step_results = {'tssc-results': {'tag-source': {}}}
 
-        run_step_test_with_result_validation(temp_dir, 'tag-source', config, expected_step_results)
+        # Using this try/finally block so that we can temporarily set environment variables for unit testing
+        _environ = dict(os.environ)  # or os.environ.copy()
+        try:
+            os.environ["GIT_USERNAME"] = "test_username"
+            os.environ["GIT_PASSWORD"] = "test_password"
+            run_step_test_with_result_validation(temp_dir, 'tag-source', config, expected_step_results, runtime_args={'repo-root': str(temp_dir.path)})
+        finally:
+            os.environ.clear()
+            os.environ.update(_environ)
--- tssc/step_implementers/tag_source/git.py
+++ tssc/step_implementers/tag_source/git.py
@@ -5,6 +5,8 @@ Step Implementer for the tag-source step for Git.
 from tssc import TSSCFactory
 from tssc import StepImplementer
 from tssc import DefaultSteps
+import sh
+import os
 
 DEFAULT_ARGS = {}
 
@@ -31,6 +33,29 @@ class Git(StepImplementer):
         """
 
     def _run_step(self, runtime_step_config):
+        username_env_var = runtime_step_config['username-env-var']
+        password_env_var = runtime_step_config['password-env-var']
+        repo_root = runtime_step_config['repo-root']
+
+        if not os.environ.get(username_env_var):
+            raise ValueError('The value provided in the step config for username-env-var, ' + username_env_var + ', is not set as a environment variable')
+        elif not os.environ.get(password_env_var):
+            raise ValueError('The value provided in the step config for password-env-var, ' + password_env_var + ', is not set as a environment variable')
+
+        # Getting the semantic version information from the hopefully previously run generate-metadata step
+        print(self.current_results())
+        generate_metadata_step_results = self.get_step_results('generate-metadata')
+        if 'version' in generate_metadata_step_results:
+            version = runtime_step_config['version']
+        else:
+            raise ValueError('The version information was not found from the generate-metadata step')
+
+        git = sh.git.bake(_cwd=repo_root)
+        git.tag(version)
+        #process1_args = 'git tag'
+        #process2_args = 'git config --get remote.origin.url'
+        #process3_args = "git push http://${ + " username_env_var + " }:${" + password_env_var + "}@gitea.apps.tssc.rht-set.com/tssc-references/tssc-reference-app-quarkus-rest-json.git --tags"
+
         results = {
         }
 
